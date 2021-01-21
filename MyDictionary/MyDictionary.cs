03eefc03c9411c76839b2e19d5e5833e3f175f6a
Oğuzhan AY <oguzhanay1998@gmail.com>
2021-01-21 22:25:05 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tx, Ty>
+    {
+        Tx[] keys;
+        Ty[] values;
+
+        public MyDictionary()
+        {
+            keys = new Tx[0];
+            values = new Ty[0];
+        }
+
+        public void Add(Tx key, Ty value)
+        {
+            Tx[] tempKeyArray = keys;
+            Ty[] temValueArray = values;
+
+            keys = new Tx[keys.Length + 1];
+            values = new Ty[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = temValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> products = new MyDictionary<int, string>();
+            products.Add(1, "Samsung");
+            products.Add(2, "Xiaomi");
+            products.Add(1, "Huawei");
+        }
+    }
+}
